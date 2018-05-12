44dcb18646bc241b655316835c1a9ce60bd7c038
Ruben Decrop <ruben@decrop.net>
2018-05-12 21:26:18 +0200

    making rd_django have a render_translate_block

--- rd_django/cms_plugins.py
+++ rd_django/cms_plugins.py
@@ -1,4 +1,13 @@
-#    Copyright 2017 - 2018 Ruben Decrop
+
+
+import logging
+log = logging.getLogger(__name__)
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext_lazy as _
+
+from .models import RdTranslationAvailable#    Copyright 2017 - 2018 Ruben Decrop
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -11,15 +20,6 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-
-import logging
-log = logging.getLogger(__name__)
-
-from cms.plugin_base import CMSPluginBase
-from cms.plugin_pool import plugin_pool
-from django.utils.translation import ugettext_lazy as _
-
-from .models import RdTranslationAvailable
 
 @plugin_pool.register_plugin
 class RdI18nPlugin(CMSPluginBase):
