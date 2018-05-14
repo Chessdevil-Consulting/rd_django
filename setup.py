def9d67d1c24728ebfc1b218cc93eb4cb285f53e
Ruben Decrop <ruben@decrop.net>
2018-05-14 08:16:03 +0200

    improced render_language_block

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = []
 
 setup(
     name='rd_django',
-    version='0.1',
+    version='0.2',
     description='Reddevil django addons',
     long_description='Reddevil django addons',
     classifiers=[
