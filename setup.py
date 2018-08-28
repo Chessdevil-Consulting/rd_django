ded185779408537f1f696358055d654711be8b07
Ruben Decrop <ruben@decrop.net>
2018-08-28 21:12:27 +0200

    version 0.4 page_title_en tag added

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = []
 
 setup(
     name='rd_django',
-    version='0.3',
+    version='0.4',
     description='Reddevil django addons',
     long_description='Reddevil django addons',
     classifiers=[
