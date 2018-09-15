7c8b374d54cb7bacf66ce96bb52de902c88b1d5d
Ruben Decrop <ruben@decrop.net>
2018-09-15 08:54:15 +0200

    building version 0.5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = []
 
 setup(
     name='rd_django',
-    version='0.4.1',
+    version='0.5',
     description='Reddevil django addons',
     long_description='Reddevil django addons',
     classifiers=[
